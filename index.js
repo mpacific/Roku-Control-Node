c487aa597fd4dee7e0bfb6141477e117e27c6bf1
Mike Pacific <mike.pacific@gmail.com>
2018-03-03 14:43:20 -0800

    CORS update for frontend use; Remove inline icon data, just return the URL to display on the frontend.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const roku = require('./lib/roku')
 
 let app = express()
 
+app.use(function (req, res, next) {
+  // TODO - Move these values to an env file
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/launch/:appId', (req, res) => {
   return roku.init(req, res, 'launchApp')
 })
